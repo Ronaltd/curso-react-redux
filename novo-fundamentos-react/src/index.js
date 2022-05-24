b662f53eec7900330fad4978c6ccc9d5e8f6bf72
Ronalt Batista <ronaltdbatista@gmail.com>
2022-05-24 08:33:01 -0300

    Componente com Parâmetro

--- novo-fundamentos-react/src/index.js
+++ novo-fundamentos-react/src/index.js
@@ -2,11 +2,15 @@ import './index.css';
 import ReactDOM from "react-dom";
 import React from 'react';
 import Primeiro from './components/basicos/Primeiro';
-
+import ComParametro from './components/basicos/ComParametro';
 
 ReactDOM.render(
-  <div>
-    <Primeiro></Primeiro>  
+  <div id="app">
+    <Primeiro></Primeiro>
+    <ComParametro
+      titulo="Situação do Aluno"
+      aluno="Pedro Silva" 
+      nota={9.3} />
   </div>,
   document.getElementById('root')
 )
