81df5d3f89391aead6f846f3a0812d1efb0216fe
Ronalt Batista <ronaltdbatista@gmail.com>
2022-05-24 08:08:35 -0300

    Primeiro Componente

---
+++ novo-fundamentos-react/src/components/basicos/Primeiro.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Primeiro () {
+  const message = 'Seja bem-vindo(a)'
+  return (
+  <div>
+    <h2>Primeiro Componente</h2>
+    <p>{message}</p>
+  </div>
+  ) 
+}
