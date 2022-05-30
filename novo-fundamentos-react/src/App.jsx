3de630372efd1558fa1b4449d7c25295363525da
Ronalt Batista <ronaltdbatista@gmail.com>
2022-05-29 22:28:32 -0300

    Estilo do Card Component

--- novo-fundamentos-react/src/App.jsx
+++ novo-fundamentos-react/src/App.jsx
@@ -11,24 +11,24 @@ import Card from "./components/layout/Card";
 export default () => (
   <div className="App">
     <h1>Fundamentos React </h1>
-    
+
     <div className="Cards">
-      <Card titulo='#04 - Desafio Aleatório'>
+      <Card titulo='#04 - Desafio Aleatório' color="#E97451">
         <Aleatorio min={1} max={60} />
       </Card>
 
-      <Card titulo="#03 - Fragmento">
+      <Card titulo="#03 - Fragmento" color="#088F8F">
         <Fragmento />
       </Card>
 
-      <Card titulo="#02 - Com Parâmetro">
+      <Card titulo="#02 - Com Parâmetro" color="#AA336A">
         <ComParametro
           titulo="Situação do Aluno"
           aluno="Pedro Silva"
           nota={9.3} />
       </Card>
 
-      <Card titulo=" #01 - Primeiro Componente">
+      <Card titulo=" #01 - Primeiro Componente" color="#E37383">
         <Primeiro></Primeiro>
       </Card>
     </div>
