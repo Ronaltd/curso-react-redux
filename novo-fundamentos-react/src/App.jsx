5dba76e45d3fe7d86fba7177d891b82d2d157518
Ronalt Batista <ronaltdbatista@gmail.com>
2022-05-29 22:14:43 -0300

    Card Component

--- novo-fundamentos-react/src/App.jsx
+++ novo-fundamentos-react/src/App.jsx
@@ -1,20 +1,36 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 
+import './App.css'
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
 
 export default () => (
-    <div id="app">
-      <h1>Fundamentos React </h1>
-      <Aleatorio min={1} max={60} />
-      <Fragmento />
-      <ComParametro
-        titulo="Situação do Aluno"
-        aluno="Pedro Silva"
-        nota={9.3} />
-      <Primeiro></Primeiro>
+  <div className="App">
+    <h1>Fundamentos React </h1>
+    
+    <div className="Cards">
+      <Card titulo='#04 - Desafio Aleatório'>
+        <Aleatorio min={1} max={60} />
+      </Card>
+
+      <Card titulo="#03 - Fragmento">
+        <Fragmento />
+      </Card>
+
+      <Card titulo="#02 - Com Parâmetro">
+        <ComParametro
+          titulo="Situação do Aluno"
+          aluno="Pedro Silva"
+          nota={9.3} />
+      </Card>
+
+      <Card titulo=" #01 - Primeiro Componente">
+        <Primeiro></Primeiro>
+      </Card>
     </div>
-  );
+  </div>
+);
